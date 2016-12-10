1b0adc98054f0880c6fa3a2ecd1bba6fde6a4186
CJMcLaughlin <Connor_McLaughlin@student.uml.edu>
2016-12-09 21:06:52 -0500

    commit2
    realized I entered in the wrong hw

--- table.js
+++ table.js
@@ -1,47 +1,35 @@
-/*
-Name: Connor McLaughlin
+/*Name: Connor McLaughlin
 CS Username: cmclaugh
 Email: Connor_McLaughlin@student.uml.edu
 Student ID: 01372970
-
 */
-
-
-/*Global for the number of tabs */
-var num_tabs = 0;
+/*When you load the page make a default table of 10x10 */
+createTable();
 
 /*Wait until the page is ready to load. */
 $(document).ready(function(){
-	
-	/*When you load the page make a default table of 10x10 */
-	
-	/*Call tab initializer */
-	$("#tabs").tabs();
-	
-
-	/*Set up data validator*/
 	$("#data").validate({
 		rules: {
 			/*Rules for all */
 			x_start: {
 				required:true,
 				digits:true,
-				range: [0, 15]
+				range: [0, 150]
 			},
 			x_end:   {
 				required: true,
 				digits: true,
-				range: [0, 15]
+				range: [0, 150]
 			},
 			y_start: {
 				required: true,
 				digits: true,
-				range: [0, 15]
+				range: [0, 150]
 			},
 			y_end:   {
 				required: true,
 				digits: true,
-				range: [0, 15]
+				range: [0, 150]
 			}
 		},
 		/*Custom messages for all inputs */
@@ -68,55 +56,22 @@ $(document).ready(function(){
 			}
 		},
 	});
-	/* Initializes Sliders */
-	init_sliders();
-
-	
-
-	/* Stops page from refreshing */	
-	$("#data").on("submit", function() {
-		return false;
-	});
-	/*If the data is valid its okay to call tabTable function  */
-	$("#data .btn").click(function(){
-		if(!$("#data").valid()){
-			return;
-		}
-		tabTable();
-	/* Delete the tab on double click */	
-		$("#tabs li").on("dblclick", function(){
-			/*Dont delete the instruction tab */
-		    if($(this).is("#instruct"))
-		    	return;
-		    /* Get its associated data and delete that too*/
-		    var id = $(this).attr('id'); 
-		    $(this).remove();
-		    $("#tabs-" + id.slice(-1)).remove();
-		    
-		});
-	});
-	
+/* Stops page from refreshing */	
+$("#data").on("submit", function() {
+	return false;
 });
-/* Adds new tab and puts in table */
-function tabTable(){
-	/*There is one more tab */
-	num_tabs++;
-	/*Sets up code to make a new tab list object */
-	var new_tab = "<li id=\"t" + num_tabs +"\"><a href=\"#tabs-" + num_tabs + "\">Table "+ num_tabs + "</a></li>";
-	/*Put that at the end of the list */
-	$("#tabs ul").append(new_tab);
-	
+/*If the data is valid its okay to call my createTable function from HW6 */
+$("#data .btn").click(function(){
+	if(!$("#data").valid()){
+		return;
+	}
+	createTable();
+});
+});
 
-	/* Add the data to the tab including the code to create the table */
-	var tab_info = "<div id=\"tabs-" + num_tabs + "\">" + createTable() + "</div>";
-	/* Refresh the tabs */
-	$("#tabs").tabs("refresh");
-
-}
 function createTable() {
 	/*Variable which will contain table element */
-	/* Before the table was part of the html but now it has to be in here for the tabs */
-	var table= "<table id=\"mult_table\" border=\"1\"><tr>";
+	var table= "<tr>";
 	/*Counters */
 	var i,j,k;
 	/* Flag indicating first runthrough */
@@ -178,59 +133,9 @@ function createTable() {
 
 	}
 
-	return table + "</table>";
-	
+	//table += "</tr>";
+	document.getElementById("mult_table").innerHTML = table;
+	//document.getElementById("demo").innerHTML = table;
 
-
-}
-
-function init_sliders(){
-	/* Set all sliders to have a min of 0 and max of 15.*/
-	/* Make sure the text box changes on slide */
-	$("#slider_x_start").slider({
-		min: 0,
-		max: 15,
-		value: parseInt($("#x_start").val()),
-		slide: function(e, ui) {
-			$("#x_start").val(ui.value);
-		}
-	});
-	$("#slider_x_end").slider({
-		min: 0,
-		max: 15,
-		value: parseInt($("#x_end").val()),
-		slide: function(e, ui) {
-			$("#x_end").val(ui.value);
-		}
-	});
-	$("#slider_y_start").slider({
-		min: 0,
-		max: 15,
-		value: parseInt($("#y_start").val()),
-		slide: function(e, ui) {
-			$("#y_start").val(ui.value);
-		}
-	});
-	$("#slider_y_end").slider({
-		min: 0,
-		max: 15,
-		value: parseInt($("#y_end").val()),
-		slide: function(e, ui) {
-			$("#y_end").val(ui.value);
-		}
-	});
-	/* Make sure the slider changes on text input */
-	$("#x_start").on("keyup", function(){
-		$("#slider_x_start").slider("value", parseInt($("#x_start").val()));
-	});
-	$("#x_end").on("keyup", function(){
-		$("#slider_x_end").slider("value", parseInt($("#x_end").val()));
-	});
-	$("#y_start").on("keyup", function(){
-		$("#slider_y_start").slider("value", parseInt($("#y_start").val()));
-	});
-	$("#y_end").on("keyup", function(){
-		$("#slider_y_end").slider("value", parseInt($("#y_end").val()));
-	});
 
 }
